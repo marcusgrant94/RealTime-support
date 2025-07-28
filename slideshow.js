e7f655d0646cba7a98a03f528a6ffb31ad82ac12
Marcus Grant <grantmarcus1994@gmail.com>
2025-07-28 19:58:46 -0400

    Added new photos, an scaled it for smaller screens

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,11 @@
 const slides = [
     'friendphoto1.JPG',
     'friendphoto2.jpg',
-    'friendphoto3.JPG'
+    'friendphoto3.JPG',
+    'friendphoto4.jpg',
+    'friendphoto5.png',
+    'friendphoto6.jpg',
+    'friendphoto7.JPG'
   ];
   let idx = 0;
   setInterval(() => {
